0a6576feaf192475b64716553b78de78277dd60b
Vyacheslav Tievsky <>
2025-03-03 23:11:07 +0300

    refactoring entrypoint

--- scripts/system/gen.mk
+++ scripts/system/gen.mk
@@ -1,5 +1,2 @@
-codegen-server:
-	@oapi-codegen --config=./testdata/openapi/oapi-codegen-server.yaml ./docs/openapi/swagger.yaml
-
-codegen-client:
-	@oapi-codegen --config=./testdata/openapi/oapi-codegen-client.yaml ./docs/openapi/swagger.yaml	
+codegen:
+	@oapi-codegen --config=./docs/oapi-codegen-server.yaml ./docs/openapi/swagger.yaml	
